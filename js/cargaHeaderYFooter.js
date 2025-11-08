1172842ff612003c53cec60053f8e9e3c775d6c1
1DAMSCojita <scojita20207@iesjoanramis.org>
2025-11-08 18:43:33 +0100

    Frontend: Separar el header y el footer en ficheros reutilizables.

---
+++ js/cargaHeaderYFooter.js
@@ -0,0 +1,25 @@
+// Creamos una función asíncrona para cargar componentes HTML
+async function cargarElemento(elementId, rutaArchivo) {
+
+    // Estructura 'try-catch'
+    // Intentará realizar la carga del archivo HTML y manejará cualquier error que ocurra durante el proceso.
+    try {
+        const response = await fetch(rutaArchivo); // Realizamos la petición para obtener el archivo HTML.
+        const html = await response.text(); // Convertimos la respuesta a texto.
+        document.getElementById(elementId).innerHTML = html; // Insertamos el HTML en el contenedor correspondiente.
+
+        // Estructura de control 'if'.
+        // Sí el objeto 'lucide' (es decir, la librería de iconos) está definido, llamamos a la función 'createIcons()' para inicializar los iconos.
+        if (typeof lucide !== 'undefined') {
+            lucide.createIcons();
+        }
+    } catch (e) {
+        console.error(`Error cargando ${rutaArchivo}:`, e);
+    }
+}
+
+// Cargar header y footer cuando la página esté lista
+document.addEventListener('DOMContentLoaded', () => {
+    cargarElemento('contenidor-header', 'views/header.html');
+    cargarElemento('contenidor-footer', 'views/footer.html');
+});
